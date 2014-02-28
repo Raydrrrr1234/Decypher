861979844ae6044074e4f3fb876794b3e6bc7bb1
DianeRay <ruikang.dai@gmail.com>
2014-02-28 11:55:25 +0800

    simple ++i learning

---
+++ C language basic test/C language basic test/clause.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	int i=0;
+	char *id = "I am here!";
+	++i;
+	return 0;
+}
